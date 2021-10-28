bdc393ed6f7ac5836a1aec20ed685f806ad0d03a
Vitaliy622 <vitalikyarick@gmail.com>
2021-10-28 16:08:26 +0300

    create CsvReport class

---
+++ src/main/java/com/oril/cryptocurrencies/model/CsvReport.java
@@ -0,0 +1,12 @@
+package com.oril.cryptocurrencies.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CsvReport {
+    String currName;
+    int maxPrice;
+    int minPrice;
+}
