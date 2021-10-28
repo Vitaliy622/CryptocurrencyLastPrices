742edb114998b075df88a9c66702e41542551a09
Vitaliy622 <vitalikyarick@gmail.com>
2021-10-28 16:09:01 +0300

    add NotFoundException

---
+++ src/main/java/com/oril/cryptocurrencies/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.oril.cryptocurrencies.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+}
